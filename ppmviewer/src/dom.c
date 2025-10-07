89d7a41f08c5a1d45b468384d2d81a8077b44607
JosueKENGNE <kengneguepouoksipierrejosue@gmail.com>
2025-10-07 17:04:05 +0100

    update

--- ppmviewer/src/dom.c
+++ ppmviewer/src/dom.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-int main(){
-    FILE *f = fopen ("/home/boyz/School_projects/TP-03/ppmviewer/Images/test2.ppm","r");
-    char ligne[100];
-    while (fgets(ligne, sizeof(ligne), f)){
-        printf("%s",ligne);
+#include <stdlib.h>
+
+int main() {
+    FILE *f = fopen("/home/boyz/School_projects/TP-03/ppmviewer/Images/test1.ppm", "r");
+    char format[3];
+    int width, height, maxval;
+
+    fscanf(f, "%2s", format);
+    fscanf(f, "%d %d", &width, &height);
+    fscanf(f, "%d", &maxval);
+
+    int r, g, b;
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            fscanf(f, "%d %d %d", &r, &g, &b);
+            printf("Pixel (%d,%d): R=%d G=%d B=%d\n", x, y, r, g, b);
+        }
     }
+    fclose(f);
+    return 0;
 }
