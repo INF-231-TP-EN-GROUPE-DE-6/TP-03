5111869ef95f848049e2c255e831cd2e54915382
Masad Shaid <shaidmasad10@gmail.com>
2025-10-10 13:31:48 +0100

    complete

--- ppmviewer/src/gris.c
+++
@@ -1,8 +0,0 @@
-#include "operations.h"
-void toGray(Image *img) {
-    for (int i = 0; i < img->hauteur; i++)
-        for (int j = 0; j < img->largeur; j++) {
-            int m = (img->pixels[i][j].r + img->pixels[i][j].g + img->pixels[i][j].b) / 3;
-            img->pixels[i][j].r = img->pixels[i][j].g = img->pixels[i][j].b = m;
-        }
-    }
